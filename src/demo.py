c849a71fd9c7a0906ad1d0379060562d76aa98e1
jaehyeon-kim <dottami@gmail.com>
2015-08-08 19:55:16 +1000

    commit after writing post

--- src/demo.py
+++ src/demo.py
@@ -1,6 +1,5 @@
 from utils.helper import Helper
 from sizmek.authentication import Auth
-from datetime import datetime
 from sizmek.advertiser import Advertiser, ConvTag
 from sizmek.campaign import Campaign
 import logging
@@ -29,42 +28,24 @@ advRes = Advertiser.GetItemRes(advertiserWSDL, auth, pageIndex=0, pageSize=50, s
 advList = Advertiser.GetItem(advRes)
 Helper.PrintObjects(advList)
 
-advListPgn = Advertiser.GetItemPgn(advertiserWSDL, auth, pageIndex=0, pageSize=50, showExtInfo=True)
-Helper.PrintObjects(advListPgn)
-
-## get conversion tags
-advId = 64920
-conRes = ConvTag.GetItemRes(advertiserWSDL, auth, advId, pageIndex=0, pageSize=50, showExtInfo=True)
-convList = ConvTag.GetItem(conRes)
-Helper.PrintObjects(convList)
-
-convListPgn = ConvTag.GetItemPgn(advertiserWSDL, auth, advId, pageIndex=0, pageSize=50, showExtInfo=True)
-Helper.PrintObjects(convListPgn)
+# advListPgn = Advertiser.GetItemPgn(advertiserWSDL, auth, pageIndex=0, pageSize=50, showExtInfo=True)
+# Helper.PrintObjects(advListPgn)
 
 ## get campaigns
-# by Id
-campaignId = 489599
-camResById = Campaign.GetItemRes(campaignWSDL, auth, pageIndex=0, pageSize=50, filterRecent=False, _id=campaignId, showExtInfo=True)
-camListById = Campaign.GetItem(camResById)
-Helper.PrintObjects(camListById)
-
-camListByIdPgn = Campaign.GetItemPgn(campaignWSDL, auth, pageIndex=0, pageSize=50, filterRecent=False, _id=campaignId, showExtInfo=True)
-Helper.PrintObjects(camListByIdPgn)
-
 # recent filter
 camRes = Campaign.GetItemRes(campaignWSDL, auth, pageIndex=0, pageSize=50, filterRecent=True, _id=None, showExtInfo=True)
 camList = Campaign.GetItem(camRes)
 Helper.PrintObjects(camList)
 
-camListPgn = Campaign.GetItemPgn(campaignWSDL, auth, pageIndex=0, pageSize=50, filterRecent=True, _id=None, showExtInfo=True)
-Helper.PrintObjects(camListPgn)
-
-Helper.WriteCsv(camList, path + 'camList.csv', 'wb')
-
-filteredList = Campaign.GetFilter(camListPgn, datetime.now().date())
-Helper.PrintObjects(filteredList)
-
-Helper.WriteCsv(camListPgn, path + 'camListPgn.csv', 'wb')
+# camListPgn = Campaign.GetItemPgn(campaignWSDL, auth, pageIndex=0, pageSize=50, filterRecent=True, _id=None, showExtInfo=True)
+# Helper.PrintObjects(camListPgn)
+# 
+# Helper.WriteCsv(camList, path + 'camList.csv', 'wb')
+# 
+# filteredList = Campaign.GetFilter(camListPgn, datetime.now().date())
+# Helper.PrintObjects(filteredList)
+# 
+# Helper.WriteCsv(camListPgn, path + 'camListPgn.csv', 'wb')
 
 
 
